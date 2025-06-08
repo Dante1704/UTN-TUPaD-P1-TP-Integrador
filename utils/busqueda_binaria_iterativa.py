0d1d0485388ad5dc491659a942d62c6c7abd5298
pabloba <pablo.arcati@nftydoor.com>
2025-06-08 00:25:43 -0300

    add time and fix some functions

--- utils/busqueda_binaria_iterativa.py
+++ utils/busqueda_binaria_iterativa.py
@@ -1,11 +1,20 @@
+import time
+
 def busqueda_binaria_iterativa(lista, objetivo):
+    inicio = time.time()
     izquierda, derecha = 0, len(lista) - 1
+    resultado = -1
+    
     while izquierda <= derecha:
         medio = (izquierda + derecha) // 2
         if lista[medio] == objetivo:
-            return medio
+            resultado = medio
+            break
         elif lista[medio] < objetivo:
             izquierda = medio + 1
         else:
             derecha = medio - 1
-    return -1
+    fin = time.time()
+    duracion = fin - inicio
+    print(f"Duración de la búsqueda: {duracion:.6f} segundos")
+    return resultado
